45e490e451def8ef6a10429fba72d93e7652e0f3
Pxdro-410 <pedrojuliocaso@gmail.com>
2025-03-24 22:00:17 -0600

    Agregar clase Node.java

---
+++ Node.java
@@ -0,0 +1,21 @@
+package HDT7;
+
+public class Node<K, V> {
+    ComparableAssociation<K, V> info;
+    Node<K, V> hijoIzquierdo, hijoDerecho;
+
+    public Node(ComparableAssociation<K, V> info) {
+        if (info == null) {
+            throw new IllegalArgumentException("El nodo no tiene datos");
+        }
+        this.info = info;
+        this.hijoIzquierdo = this.hijoDerecho = null;
+    }
+    
+
+    @Override
+    public String toString() {
+        return "[Clave: " + info.key + ", Valor: " + info.value + "]";
+    }
+
+}
